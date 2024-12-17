dd90d4715787a79d4acc0971a6ba06dc0eca8dcb
DreamyBlueLunar <2010614119@qq.com>
2024-12-17 16:41:09 +0000

    update logger's unit test

--- leemuduo/logger.h
+++ leemuduo/logger.h
@@ -6,8 +6,6 @@
 
 #define LOG_BUF_LEN 1024
 
-#define MUDEBUG
-
 // C 库函数 int snprintf(char *str, size_t size, const char *format, ...) 
 // 设将可变参数(...)按照 format 格式化成字符串，并将字符串复制到 str 中，
 // size 为要写入的字符的最大数目，超过 size 会被截断，最多写入 size-1 个字符。
