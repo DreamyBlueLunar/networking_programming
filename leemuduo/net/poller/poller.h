ab082995c4e05c61c4ecef4042548aac58d73b87
DreamyBlueLunar <168215331+DreamyBlueLunar@users.noreply.github.com>
2024-12-19 16:59:01 +0800

    Feature/sample (#11)
    * modified some parameters
    
    * move to another repo

--- leemuduo/net/poller/poller.h
+++ leemuduo/net/poller/poller.h
@@ -18,16 +18,16 @@ public:
     virtual ~poller() = default;
 
     // 封装 epoll_wait、poll 等 I/O 复用接口，线程中必须调用这个接口实现事件循环
-    virtual time_stamp poll_wait(int, channel_list*) = 0;
+    virtual time_stamp poll_wait(int timeout_ms, 
+                                    channel_list* active_channels) = 0;
     // 更新感兴趣的事件，线程中必须调用这个接口
-    virtual void update_channel(channel*) = 0;
+    virtual void update_channel(channel* chann) = 0;
     // 删除事件，线程中必须调用这个接口
-    virtual void remove_channel(channel*) = 0;
-    
+    virtual void remove_channel(channel* chann) = 0;
     // 检查 channels_ 中有没有这个 channel
-    virtual bool has_channel(channel*) const;
+    virtual bool has_channel(channel* chann) const;
 
-    static poller* new_default_poller(event_loop*);
+    static poller* new_default_poller(event_loop* loop);
 
 protected:
     using channel_map = std::map<int, channel*>;
